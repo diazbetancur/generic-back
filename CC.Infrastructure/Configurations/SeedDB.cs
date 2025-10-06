11ec51add311d84225cc48393f1e0257c27630ca
Juan Pablo <diazbetancur@gmail.com>
2025-10-06 06:58:28 -0500

    test

--- CC.Infrastructure/Configurations/SeedDB.cs
+++ CC.Infrastructure/Configurations/SeedDB.cs
@@ -3,15 +3,14 @@ namespace CC.Infrastructure.Configurations;
 public class SeedDB
 {
   private readonly DBContext _context;
+
   public SeedDB(DBContext context)
   {
     _context = context;
-
   }
 
   public async Task SeedAsync()
   {
     await _context.Database.EnsureCreatedAsync();
-
   }
 }
